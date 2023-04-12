faea2f2176e4db6907e81fbdb0210d0e35a3b96c
bhuism <bhuism@odee.net>
2023-04-12 10:59:47 +0200

    first commit

---
+++ src/test/java/nl/appsource/ok/OkApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.appsource.ok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
