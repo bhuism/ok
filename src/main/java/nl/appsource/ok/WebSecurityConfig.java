3e9110531388e7af3fbb9c3ffca1083fab5c44c0
bhuism <bhuism@odee.net>
2023-04-24 11:53:45 +0200

    add security

---
+++ src/main/java/nl/appsource/ok/WebSecurityConfig.java
@@ -0,0 +1,24 @@
+package nl.appsource.ok;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.NEVER))
+            .authorizeHttpRequests((requests) -> requests
+                .anyRequest().permitAll()
+            );
+        return http.build();
+    }
+
+}
