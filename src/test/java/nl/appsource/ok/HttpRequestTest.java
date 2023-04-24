27478299a00c10c177c9a4b73f38c669ec33f3b1
bhuism <bhuism@odee.net>
2023-04-24 10:35:38 +0200

    fix test

--- src/test/java/nl/appsource/ok/HttpRequestTest.java
+++ src/test/java/nl/appsource/ok/HttpRequestTest.java
@@ -1,7 +1,5 @@
 package nl.appsource.ok;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,29 +7,30 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class HttpRequestTest {
 
     @Value(value = "${local.server.port}")
     private int serverPort;
 
-//    @Value(value = "${management.server.port}")
-    private int managementPort = 9080;
-
+    //    @Value(value = "${management.server.port}")
+    private final int managementPort = 9080;
 
     @Autowired
     private TestRestTemplate restTemplate;
 
     @Test
-    public void greetingShouldReturnDefaultMessage() throws Exception {
+    public void greetingShouldReturnDefaultMessage() {
         assertThat(this.restTemplate.getForObject("http://localhost:" + serverPort + "/",
-                String.class)).isEqualTo("OK");
+            String.class)).isEqualTo("OK");
     }
 
     @Test
-    public void actuatorHealthShouldReturnDefaultMessage() throws Exception {
+    public void actuatorHealthShouldReturnDefaultMessage() {
         assertThat(this.restTemplate.getForObject("http://localhost:" + managementPort + "/actuator/health",
-            String.class)).isEqualTo("{\"status\":\"UP\"}");
+            String.class)).isEqualTo("{\"status\":\"UP\",\"groups\":[\"liveness\",\"readiness\"]}");
     }
 
 }
