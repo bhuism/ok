65be5dcf28a2b60090bb699e595cfc37fd4d77d7
bhuism <bhuism@odee.net>
2023-07-28 23:42:13 +0200

    cleanups

--- src/main/java/nl/appsource/ok/OkController.java
+++ src/main/java/nl/appsource/ok/OkController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class OkController {
 
     @RequestMapping("/")
-    String home() {
+    public String home() {
         return "OK";
     }
 
