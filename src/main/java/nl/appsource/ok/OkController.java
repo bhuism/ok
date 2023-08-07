123685cd8c608ac4373225c4102f4d4ea916279f
bhuism <bhuism@odee.net>
2023-08-07 19:44:58 +0200

    add time

--- src/main/java/nl/appsource/ok/OkController.java
+++ src/main/java/nl/appsource/ok/OkController.java
@@ -1,26 +1,42 @@
 package nl.appsource.ok;
 
 import jakarta.servlet.http.HttpServletRequest;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Optional;
+import java.util.Set;
+import java.util.function.Function;
 
-@Slf4j
+import static java.time.format.DateTimeFormatter.ISO_OFFSET_DATE_TIME;
+
 @RestController
 public class OkController {
+
+    private static final Set<String> AVAILABLE_ZONE_IDS = ZoneId.getAvailableZoneIds();
+
+    private static final Function<Optional<String>, Optional<String>> mytime = (zone) -> Optional.of(ISO_OFFSET_DATE_TIME.format(LocalDateTime.now().atZone(
+        zone
+            .filter(AVAILABLE_ZONE_IDS::contains)
+            .map(ZoneId::of)
+            .orElse(ZoneId.systemDefault())
+    )));
+    public static final String X_ORIGINAL_FORWARDED_FOR = "x-original-forwarded-for";
 
     @GetMapping("/")
     public ResponseEntity<String> home(
         final HttpServletRequest httpServletRequest,
-        @RequestHeader("x-original-forwarded-for") final Optional<String> originalForwardedFor
+        @RequestHeader(X_ORIGINAL_FORWARDED_FOR) final Optional<String> originalForwardedFor,
+        @RequestParam final Optional<String> zone
     ) {
 
         final ServerHttpRequest request = new ServletServerHttpRequest(httpServletRequest);
@@ -29,6 +45,7 @@ public class OkController {
         return Optional.ofNullable(uriComponents.getHost()).flatMap(name -> switch (name) {
                 case "ok.impl.nl" -> Optional.of("OK");
                 case "ip.impl.nl" -> originalForwardedFor;
+                case "time.impl.nl" -> mytime.apply(zone);
                 default -> Optional.empty();
             })
             .map(ResponseEntity::ok)
--- src/test/java/nl/appsource/ok/HttpRequestTest.java
+++ src/test/java/nl/appsource/ok/HttpRequestTest.java
@@ -10,6 +10,9 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 
+import java.util.Objects;
+
+import static java.time.format.DateTimeFormatter.ISO_OFFSET_DATE_TIME;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
@@ -35,6 +38,20 @@ public class HttpRequestTest {
                 String.class
             ).getBody()
         ).isEqualTo("OK");
+    }
+
+    @Test
+    public void greetingShouldReturnTimeMessage() {
+        final HttpHeaders requestHeaders = new HttpHeaders();
+        requestHeaders.set("x-forwarded-host", "time.impl.nl");
+        final HttpEntity<String> entity = new HttpEntity<>(requestHeaders);
+        assertThat(
+            ISO_OFFSET_DATE_TIME.parse(
+                Objects.requireNonNull(this.restTemplate.exchange("http://localhost:" + serverPort + "/", HttpMethod.GET,
+                    entity,
+                    String.class
+                ).getBody())
+            )).isNotNull();
     }
 
     @Test
