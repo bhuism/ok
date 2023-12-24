cf338d77de324cccc9699d9a84566a7254a1a4d7
bhuism <bhuism@odee.net>
2023-12-24 22:15:36 +0100

    show git hash in startuplog

--- src/main/java/nl/appsource/ok/EventListener.java
+++ src/main/java/nl/appsource/ok/EventListener.java
@@ -1,17 +1,22 @@
 package nl.appsource.ok;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.info.GitProperties;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class EventListener implements ApplicationListener<ApplicationReadyEvent> {
+
+    private final GitProperties gitProperties;
 
     @Override
     public void onApplicationEvent(final ApplicationReadyEvent event) {
-        log.info(event.getClass().getSimpleName());
+        log.info(event.getClass().getSimpleName() + ": " + gitProperties.getShortCommitId());
     }
 
 }
